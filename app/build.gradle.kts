e73a8f4d2e773d616da2e643f61fc3f8226fca23
JCdomt <jcdomt@163.com>
2026-03-25 13:34:22 +0800

    version: 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.wzjer.fuckcg"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10000
-        versionName = "1.0.0"
+        versionCode = 11000
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
